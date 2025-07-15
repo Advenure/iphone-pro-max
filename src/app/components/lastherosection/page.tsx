05189a9b4794ec934eee56bc6737a3d4496fd08c
Advenure <kkamrantasleem@gmail.com>
2025-07-15 20:55:26 +0500

    Update page.tsx

--- src/app/components/lastherosection/page.tsx
+++ src/app/components/lastherosection/page.tsx
@@ -1,13 +1,18 @@
 
-export default function HeroSectionLast (){
-    return(
-        <div className="bg-[url('/lastmobile.png')] bg-cover bg-center h-[75vh] w-full  md:bg-[url('/lastherosection.png')] md:bg-cover md:bg-center md:w-full md:h-[50vh] mt-2 flex justify-center items-center">
-            <div className="place-content-center place-items-center w-full px-3">
-        <h1 className="text-6xl  text-center  md:text-7xl text-[#FFFFFF]">Big Summer  <span className="text-6xl   md:text-8xl font-bold text-[#FFFFFF]">Sale</span> </h1>
-        <p className=" text-xl text-center md:text-2xl text-[#787878] mt-2">Commodo fames vitae vitae leo mauris in. Eu consequat.</p>
-        <button className="px-16 mt-4 py-4 text-lg md:mt-2 md:px-20 md:py-5 md:text-2xl semi-bold rounded-md border border-white text-white focus-none">Shop Now</button>
+export default function HeroSectionLast () {
+    return (
+        <div className="bg-[url('/lastmobile.png')] bg-cover bg-center h-[75vh] w-full md:bg-[url('/lastherosection.png')] md:bg-cover md:bg-center md:w-full md:h-[50vh] mt-2 flex justify-center items-center">
+            <div className="flex flex-col items-center justify-center w-full px-3">
+                <h1 className="text-6xl text-center md:text-7xl text-[#FFFFFF]">
+                    Big Summer <span className="text-6xl md:text-8xl font-bold text-[#FFFFFF]">Sale</span>
+                </h1>
+                <p className="text-xl text-center md:text-2xl text-[#787878] mt-2">
+                    Commodo fames vitae vitae leo mauris in. Eu consequat.
+                </p>
+                <button className="px-16 mt-4 py-4 text-lg md:mt-2 md:px-20 md:py-5 md:text-2xl font-semibold rounded-md border border-white text-white focus-none mx-auto">
+                    Shop Now
+                </button>
             </div>
-
         </div>
-    )
+    );
 }
